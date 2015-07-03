bba8284427926da2f4ab19a26e8b6b9bf99d44e3
fajarizaf <fajarizaf@gmail.com>
2015-07-03 16:35:22 +0700

    update header pada setiap template

--- application/views/templates/default/foot.php
+++ application/views/templates/default/foot.php
@@ -1,8 +1,24 @@
 <section class="foot" element="section" resizelayout="false">
 <div class="moveSection"></div>
 </section>
+<script>
+    $('body').show();
+    $('.version').text(NProgress.version);
+    NProgress.start();
+    setTimeout(function() { NProgress.done(); $('.fade').removeClass('out'); }, 1000);
 
+    $("a").click(function(e) {
+    	e.preventDefault();
+    	NProgress.start();
+    	var menu = $(this).attr('href');
+    	$('.scrat').load(menu, function() {
+    		NProgress.done();
+    	});
+    });
 
+  </script>
+
+  <script>var HN=[];HN.factory=function(e){return function(){HN.push([e].concat(Array.prototype.slice.call(arguments,0)))};},HN.on=HN.factory("on"),HN.once=HN.factory("once"),HN.off=HN.factory("off"),HN.emit=HN.factory("emit"),HN.load=function(){var e="hn-button.js";if(document.getElementById(e))return;var t=document.createElement("script");t.id=e,t.src="//hn-button.herokuapp.com/hn-button.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)},HN.load();</script>
 </div>
 </body>
 </html>
--- application/views/templates/default/header.php
+++ application/views/templates/default/header.php
@@ -8,7 +8,10 @@
 <link href="<?php echo base_url(); ?>templates/default/css/ElementStyleButton.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url(); ?>templates/default/css/ElementStyleLayout.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url(); ?>templates/default/css/ElementStyleMenu.css" rel="stylesheet" type="text/css" />
-
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<link href='<?php echo base_url(); ?>templates/default/css/nprogress.css' rel='stylesheet' />
+<script src='<?php echo base_url(); ?>templates/default/js/nprogress.js'></script>
+<script>if(location.hostname.match(/ricostacruz\.com$/)){var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-20473929-1"]),_gaq.push(["_trackPageview"]),function(){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)}()}</script>
 
 </head>
     
